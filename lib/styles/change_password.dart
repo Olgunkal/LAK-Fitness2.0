0f2c26915d37af3ce8660bf7b0d70ec292d0cb7a
mrbozna38 <muhammedbozna@gmail.com>
2023-09-29 16:23:14 +0200

    Passwort ändern

--- lib/styles/change_password.dart
+++ lib/styles/change_password.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:lak_fitness/styles/color.dart';
 
@@ -11,21 +13,40 @@ class ChangePassword extends StatefulWidget {
 
 class _ChangePasswordState extends State<ChangePassword> {
   final GlobalKey<FormState> formKey = GlobalKey();
-  final passwordController = TextEditingController();
+  final oldPasswordController = TextEditingController();
+  final newPasswordController = TextEditingController();
   final confirmedController = TextEditingController();
   late String password;
   late String confirmedPassword;
 
+  var auth = FirebaseAuth.instance;
+  final currentUser = FirebaseAuth.instance.currentUser;
+
   @override
   void dispose() {
-    passwordController.dispose();
+    oldPasswordController.dispose();
+    newPasswordController.dispose();
     confirmedController.dispose();
     super.dispose();
   }
 
-  void save() {
-    if (formKey.currentState!.validate()) {
-      widget.changePassword(password);
+  Future<void> save() async {
+    // TODO Check old password
+
+    if (newPasswordController.text != confirmedController.text) {
+      // TODO Error
+      return;
+    }
+
+    final user = FirebaseAuth.instance.currentUser;
+    final cred = EmailAuthProvider.credential(
+        email: currentUser!.email!, password: oldPasswordController.text);
+
+    try {
+      await user!.reauthenticateWithCredential(cred);
+      await user.updatePassword(newPasswordController.text);
+    } finally {
+      Navigator.pop(context);
     }
   }
 
@@ -43,7 +64,31 @@ class _ChangePasswordState extends State<ChangePassword> {
             mainAxisSize: MainAxisSize.min,
             children: [
               TextFormField(
-                controller: passwordController,
+                controller: oldPasswordController,
+                style: Theme.of(context).textTheme.bodyMedium,
+                decoration: InputDecoration(
+                  border: const OutlineInputBorder(
+                    borderSide: BorderSide(),
+                    borderRadius: BorderRadius.all(Radius.circular(6)),
+                  ),
+                  hintText: 'altes Passwort eingeben',
+                  labelText: 'altes Passwort*',
+                  labelStyle: Theme.of(context).textTheme.titleSmall,
+                  floatingLabelBehavior: FloatingLabelBehavior.always,
+                  suffixIcon: const Icon(Icons.remove_red_eye),
+                ),
+                onChanged: (String txt) => password = txt,
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Passwort ungültig';
+                  } else {
+                    return null;
+                  }
+                },
+              ),
+              const SizedBox(height: 25.0),
+              TextFormField(
+                controller: newPasswordController,
                 style: Theme.of(context).textTheme.bodyMedium,
                 decoration: InputDecoration(
                   border: const OutlineInputBorder(
@@ -60,7 +105,6 @@ class _ChangePasswordState extends State<ChangePassword> {
                   helperMaxLines: 2,
                 ),
                 onChanged: (String txt) => password = txt,
-                onFieldSubmitted: (String txt) => save(),
                 validator: (value) {
                   if (value!.isEmpty) {
                     return 'Passwort ungültig';
@@ -69,7 +113,7 @@ class _ChangePasswordState extends State<ChangePassword> {
                   }
                 },
               ),
-              const SizedBox(height: 32.0),
+              const SizedBox(height: 19.0),
               TextFormField(
                 controller: confirmedController,
                 style: Theme.of(context).textTheme.bodyMedium,
@@ -84,9 +128,8 @@ class _ChangePasswordState extends State<ChangePassword> {
                   suffixIcon: const Icon(Icons.remove_red_eye),
                 ),
                 onChanged: (String txt) => confirmedPassword = txt,
-                onFieldSubmitted: (String txt) => save(),
                 validator: (value) {
-                  if (value!.isEmpty || value != passwordController.text) {
+                  if (value!.isEmpty || value != newPasswordController.text) {
                     return 'Passwort stimmt nicht über ein';
                   } else {
                     return null;
